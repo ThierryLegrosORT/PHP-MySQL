461ab65fd926158bc9ec5559831ab50572934e69
Thierry Legros <thierry.legros2@club.fr>
2019-07-30 14:32:28 +0200

    bases sur les tableaux - mise en forme du code

--- tableaux.php
+++ tableaux.php
@@ -1,7 +1,10 @@
 <?php
 
 $noms = array("Thierry", "Fanny","Léa");
-$personne = array("nom" => "Nebra", "prenom" => "Bob", "age" => 99);
+$personne = array("nom" => "Nebra",
+ "prenom" => "Bob",
+  "age" => 99);
+
 echo "<br>";
 
 print_r($personne); // affiche le contenu du tableau
@@ -10,23 +13,28 @@ print_r($personne); // affiche le contenu du tableau
 for($numero = 0; $numero<3; $numero++){
     echo "<p>" . $noms[$numero] . "</p>";
 }
+
 echo "<br>";
+
 // meme chose qu'au dessus, mais sans avoir besoin de connaitre la longueur du tableau
 foreach($noms as $nom){
     echo "<p>" . $nom . "</p>";
 }
+
 echo "<br>";
 
 // meme chose sur le tableau personne
 foreach($personne as $detail){
     echo "<p>" . $detail . "</p>";
 }
+
 echo "<br>";
 
 // methode permettant de recuperer le libelle de la valeur affichée
 foreach($personne as $libelle => $detail){
     echo "<p>" . $libelle . " vaut " . $detail . "</p>";
 }
+
 echo "<br>";
 
 ?>
