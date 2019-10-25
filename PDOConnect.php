58f1f7465c6d4183307c420b92136ac7f7d3f8e9
Thierry LEGROS <thierry.legros2@club.fr>
2019-10-25 11:50:59 +0200

    initialisation de la connexion BDD avec test d'erreur try-catch

---
+++ PDOConnect.php
@@ -0,0 +1,7 @@
+<?php
+try{ // on teste la présence d'erreur
+    $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', ''); // exemple de connexion avec l'objet PDO (ici pour une connexion locale à mysql, sans mot de passe)
+}
+catch (Exception $e){ // si une erreur est détectée, le message suivant est affiché
+    die('Erreur: ' . $e->getMessage());
+}
