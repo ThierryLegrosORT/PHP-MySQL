f4764ea19ff7ecbdf9001844409f1212388c9b0b
Thierry LEGROS <thierry.legros2@club.fr>
2019-10-27 00:30:34 +0200

    activation des erreurs à la connexion PDO

--- PDOConnect.php
+++ PDOConnect.php
@@ -3,8 +3,8 @@
 NE PAS AJOUTER CE FICHIER DANS LE REPO SI IL CONTIENT DE VRAIS IDENTIFIANTS
 */
 try{ // on teste la présence d'erreur
-    $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', ''); // exemple de connexion avec l'objet PDO (ici pour une connexion locale à mysql, sans mot de passe)
-    // echo "connexion reussie !";
+    // $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', ''); // exemple de connexion avec l'objet PDO (ici pour une connexion locale à mysql, sans mot de passe)
+    $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)); //activation des erreurs à la connexion pour afficher un detail sur l'erreur
 }
 catch (Exception $e){ // si une erreur est détectée, le message suivant est affiché
     die('Erreur: ' . $e->getMessage());
