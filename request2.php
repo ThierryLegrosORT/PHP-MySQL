107860f75940e1115d2815c3538c7483997b3824
Thierry LEGROS <thierry.legros2@club.fr>
2019-10-26 13:58:54 +0200

    init prepared request

--- request2.php
+++ request2.php
@@ -1,10 +1,17 @@
 <?php
 require_once('PDOConnect.php');
 
-$response = $bdd->query('SELECT nom FROM jeux_video');
+// $response = $bdd->query('SELECT nom, id FROM jeux_video');
 
+// while($data = $response->fetch()){
+//     echo $data['id'] . '- ' .$data['nom'] . '<br>';
+// }
+
+// $response->closeCursor();
+
+$response = $bdd->query('SELECT nom, prix, possesseur FROM jeux_video WHERE possesseur=\'Patrick\' ORDER BY prix');
 while($data = $response->fetch()){
-    echo $data['nom'] . '<br>';
+    echo '<strong>' . $data['nom'] . '</strong> appartient à <strong>' . $data['possesseur'] . '</strong> et coûte ' . $data['prix'] . ' euro(s).<br><br>';
 }
 
 $response->closeCursor();
