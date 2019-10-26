4f73ad2e7d9b88c80616083b73676d60e2ecc813
Thierry LEGROS <thierry.legros2@club.fr>
2019-10-27 00:24:51 +0200

    requetes preparees avec marqueurs nominatifs

--- prepared_request.php
+++ prepared_request.php
@@ -3,8 +3,10 @@ require_once('header.php');
 require_once('menu.php');
 require_once('PDOConnect.php');
 
-$req = $bdd->prepare('SELECT nom, prix FROM jeux_video WHERE possesseur = ? AND prix <= ?');
-$req->execute(array($_POST['possesseur'], $_POST['prix_max']));
+// $req = $bdd->prepare('SELECT nom, prix FROM jeux_video WHERE possesseur = ? AND prix <= ?');
+// $req->execute(array($_POST['possesseur'], $_POST['prix_max']));
+$req = $bdd->prepare('SELECT nom, prix FROM jeux_video WHERE possesseur = :possesseur AND prix <= :prixmax');
+$req->execute(array('possesseur'=>$_POST['possesseur'], 'prixmax'=>$_POST['prix_max']));
 
 echo 'Résultat pour ' . $_POST['possesseur'] . ' avec un prix max de ' . $_POST['prix_max'] . ' EUR.';
 echo '<ul>';
