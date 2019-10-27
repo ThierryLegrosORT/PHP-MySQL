fbba5e875d97f05f8cdf2daae54dd3c62f9d8c65
Thierry LEGROS <thierry.legros2@club.fr>
2019-10-27 23:50:03 +0100

    Minichat basique

--- minichat.php
+++ minichat.php
@@ -1,6 +1,18 @@
 <?php
 require_once('header.php');
 require_once('menu.php');
+require_once('PDOConnect.php');
+
+$pseudo = htmlspecialchars($_POST['pseudo']);
+$message = htmlspecialchars($_POST['message']);
+
+$req = $bdd->prepare('INSERT INTO minichat(pseudo, message) VALUES(:pseudo, :message)');
+$req->execute(array(
+    'pseudo' => $pseudo,
+    'message' => $message
+));
+
+header('Location: minichat_form.php');
 
 
 require_once('footer.php');
--- minichat_form.php
+++ minichat_form.php
@@ -1,6 +1,43 @@
 <?php
 require_once('header.php');
 require_once('menu.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
 
+<body>
+    <form action="minichat.php" method="post">
+        <label for="pseudo">Votre pseudo: </label>
+        <input type="text" name="pseudo" minlength="4" required><br>
+        <label for="message">Votre message: </label>
+        <textarea name="message" cols="30" rows="4" minlength="3" required></textarea>
+        <input type="submit" value="Envoyer">
+    </form>
+
+    <?php
+    require_once('PDOConnect.php');
+    $response = $bdd->query('SELECT pseudo, message FROM minichat ORDER BY id DESC limit 0, 10');
+    while ($data = $response->fetch()) {
+        $pseudo = htmlspecialchars($data['pseudo']);
+        $message = htmlspecialchars($data['message']);
+        ?>
+        <p>
+            <?php echo '<strong>' . $pseudo . '</strong>: ' . $message; ?><br>
+        </p>
+    <?php
+    }
+    $response->closeCursor();
+    ?>
+</body>
+
+</html>
+
+<?php
 require_once('footer.php');
