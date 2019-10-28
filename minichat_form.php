d2094c7d05483debdc725eb3b8bbc75a20519842
Thierry LEGROS <thierry.legros2@club.fr>
2019-10-28 13:26:07 +0100

    Amélioration minichat #1

--- minichat_form.php
+++ minichat_form.php
@@ -13,30 +13,38 @@ require_once('menu.php');
 </head>
 
 <body>
-    <form action="minichat.php" method="post">
-        <label for="pseudo">Votre pseudo: </label>
-        <input type="text" name="pseudo" minlength="4" required><br>
-        <label for="message">Votre message: </label>
-        <textarea name="message" cols="30" rows="4" minlength="3" required></textarea>
-        <input type="submit" value="Envoyer">
-    </form>
+    <div class="chatbox">
 
-    <?php
-    require_once('PDOConnect.php');
-    $response = $bdd->query('SELECT UPPER(pseudo) AS pseudo_maj, message, DATE_FORMAT(date_ajout, "%d/%m/%y %Hh%i") as heure_ajout FROM minichat ORDER BY id DESC limit 0, 10');
-    while ($data = $response->fetch()) {
-        $pseudo = htmlspecialchars($data['pseudo_maj']);
-        $message = htmlspecialchars($data['message']);
-        $date_ajout = $data['heure_ajout'];
+        <div class="formulaire_chat">
+            <fieldset>
+                <form action="minichat.php" method="post">
+                    <label for="pseudo">Votre pseudo: </label>
+                    <input type="text" name="pseudo" minlength="4" required> 4 car. min.<br>
+                    <label for="message">Votre message: </label>
+                    <textarea name="message" cols="30" rows="4" minlength="3" required></textarea>
+                    <input type="submit" value="Envoyer">
+                </form>
+            
+            </fieldset>
+        </div>
+            <div class="minichat">
+        <?php
+        require_once('PDOConnect.php');
+        $response = $bdd->query('SELECT UPPER(pseudo) AS pseudo_maj, message, DATE_FORMAT(date_ajout, "%d/%m/%y %Hh%i") as heure_ajout FROM minichat ORDER BY id DESC limit 0, 10');
+        while ($data = $response->fetch()) {
+            $pseudo = htmlspecialchars($data['pseudo_maj']);
+            $message = htmlspecialchars($data['message']);
+            $date_ajout = $data['heure_ajout'];
+            ?>
+            <p class="msg_box">
+                <?php echo $date_ajout . ': <strong>' . $pseudo . '</strong>: ' . $message; ?><br>
+            </p>
+        <?php
+        }
+        $response->closeCursor();
         ?>
-        <div class="minichat">
-        <p>
-            <?php echo $date_ajout . ': <strong>' . $pseudo . '</strong>: ' . $message; ?><br>
-        </p></div>
-    <?php
-    }
-    $response->closeCursor();
-    ?>
+        </div>
+    </div>
 </body>
 
 </html>
